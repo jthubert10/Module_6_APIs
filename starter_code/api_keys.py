2a59d6923ce0925c2356b4802072a70134317dfa
Jared Hubert <jthubert10@gmail.com>
2023-01-31 12:45:43 -0600

    Initial

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "5aa50a12cf26c6fe43e9937ac656bb1a"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "e9fd53140ca84ca38f951af802480e0f"
